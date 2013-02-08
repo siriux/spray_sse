dac489e4bd1b56f0ea8e0386526fcfafd7605747
Pablo Guerrero <siriux@gmail.com>
2013-02-08 16:57:27 +0100

    Make the interface closer to websockets.

--- src/main/resources/sse.js
+++ src/main/resources/sse.js
@@ -3,36 +3,53 @@
 function SSE(url) {
   this.url = url;
   this.initialized = false;
-  this.onMessage = function(e){ console.log(e); }; // Default handler logs
 
   var self = this;
 
-  this.evtSource = new EventSource(this.url + "/receive");
-  this.evtSource.addEventListener("init", function(msg) {
+  evtSource = new EventSource(this.url + "/receive");
+  evtSource.addEventListener("init", function(msg) {
     var jsonMsg = JSON.parse(msg.data);
     self.id = jsonMsg.id;
     self.token = jsonMsg.token;
     self.initialized = true;
-    if (self.onInit) {
-      self.onInit()
+    if (self.onopen) {
+      self.onopen()
     }
   });
 
-  this.evtSource.addEventListener("message", function(msg){ self.onMessage(msg.data); });
+  evtSource.addEventListener("message", function(msg){
+    if (self.onmessage) {
+      self.onmessage(msg);
+    }
+  });
+
+  evtSource.onerror = function(e){
+    if (self.onerror) {
+        self.onerror(e);
+    }
+  };
+
+  this.evtSource = evtSource;
 }
 
 SSE.prototype.send = function(msg) {
   if (this.initialized) {
-    var request = new XMLHttpRequest();
-     
-    request.open("POST", this.url + "/send", true);
-    request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    request.send("id=" + this.id + "&token=" + this.token + "&msg=" + msg);
-    // TODO handle errors here
+    var req = new XMLHttpRequest();
+    req.addEventListener("error", function(e) {
+      if (self.onerror) {
+        self.onerror(e);
+      }
+    }, false); 
+    req.open("POST", this.url + "/send", true);
+    req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    req.send("id=" + this.id + "&token=" + this.token + "&msg=" + msg);
+
   } else {
     console.log("Not initialized")
   }
 }
+
+SSE.prototype.close = function() { this.evtSource.close(); }
 
 window.SSE = SSE;
 
